5368345c90d58ecf172b98277c1e5ae33e8a676e
lykos98 <francesco.tomba17@gmail.com>
2023-02-08 22:54:40 +0100

    added argv to choose z

--- driver.c
+++ driver.c
@@ -23,8 +23,8 @@ int main(int argc, char** argv){
     {
         Z = atof(argv[3]);
         halo = atoi(argv[4]);
-        if(halo != 0 || halo != 1){
-            printf("Insert valid halo identifier: 0 do not assign halo, 1 assign particles to the halo");
+        if(halo != 0 && halo != 1){
+            printf("Insert valid halo identifier: 0 do not assign halo, 1 assign particles to the halo\n");
             return;
         }
     }
