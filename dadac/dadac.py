110527018f2a4f9179b71bd77544872bba9cdb04
lykos98 <francy273998@gmail.com>
2024-01-19 17:09:34 +0100

    removed unnecessary print

--- dadac/dadac.py
+++ dadac/dadac.py
@@ -81,7 +81,7 @@ class Clusters(ct.Structure):
 class _dadac_loader():
     def __init__(self):
         path = os.path.join(os.path.dirname(__file__), "bin/libdadac.so")
-        print(path)
+        #print(path)
         if not os.path.exists(path):
             print("dadac is not built yet, calling make for you")
             try:
