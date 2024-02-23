ff8ecec1412b7353c84e8bd5d042405f250476ea
lykos98 <francy273998@gmail.com>
2024-02-23 19:29:39 +0100

    trying to install to macos

--- dadac/__init__.py
+++ dadac/__init__.py
@@ -1,1 +1,4 @@
+import platform
+if platform.platform() != "Linux":
+    raise(ModuleNotFoundError(f"Implementation actually supports only Linux, you are running on {platform.platform()} "))
 from .dadac import *
--- dadac/macos_compile.sh
+++ dadac/macos_compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-${CC} src/dadac.c src/kdtree.c src/kdtreeV2.c src/heap.c src/vptree.c src/vptreeV2.c -Wall -Wextra -O3 -lm -fopenmp -I./include -fpic -shared -o bin/libdadac.so 
+clang src/dadac.c src/kdtree.c src/kdtreeV2.c src/heap.c src/vptree.c src/vptreeV2.c -Wall -Wextra -O3 -lm -fopenmp=libomp -I./include -fpic -shared -o bin/libdadac.so 
 
 
