ba55aba4133ddcdc1b8f99226e49bdbd555459de
lykos98 <francy273998@gmail.com>
2024-02-23 19:18:49 +0100

    trying to install to macos

--- dadac/macos_compile.sh
+++ dadac/macos_compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc src/dadac.c src/kdtree.c src/kdtreeV2.c src/heap.c src/vptree.c src/vptreeV2.c -Wall -Wextra -O3 -lm -fopenmp -I./include -fpic -shared -o bin/libdadac.so 
+${CC} src/dadac.c src/kdtree.c src/kdtreeV2.c src/heap.c src/vptree.c src/vptreeV2.c -Wall -Wextra -O3 -lm -fopenmp -I./include -fpic -shared -o bin/libdadac.so 
 
 
